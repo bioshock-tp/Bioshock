237af42bd9f0491d0d1def5e9546790103e918bb
LukeAmos00 <l.amos00@hotmail.co.uk>
2021-02-15 22:10:53 +0000

    Added basic functionality

--- src/main/App.java
+++ src/main/App.java
@@ -1,5 +1,68 @@
-public class App {
-    public static void main(String[] args) {
-        run();
+package org.bioshock;
+
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.stage.Stage;
+
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private Pane root = new Pane();
+    private Sprite player = new Sprite(300,400,40,40,200,Color.PINK);
+    private Enemy enemy = new Enemy(10, 10, 40,40,300,Color.INDIANRED);
+
+    private Parent buildContent(){
+        root.setStyle("-fx-background-color: black");
+        root.setPrefSize(600,800);
+
+
+        root.getChildren().addAll(player, enemy);
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long l) {
+                update();
+            }
+        };
+
+        timer.start();
+
+        return root;
     }
+
+    private void update(){
+        Shape intersects = Shape.intersect(enemy.fov, player.spr);
+        if(intersects.getBoundsInLocal().getWidth() != -1){
+            enemy.followPlayer(player);
+        }
+    }
+
+    @Override
+    public void start(Stage stage) {
+        Scene scene = new Scene(buildContent());
+
+        scene.setOnMouseClicked(e -> {
+            player.setCentreXY(e.getX(), e.getY());
+        });
+
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
