c772eba9052cdedbbeff864fad847cec52b6954f
WillHolbrook <William.d.holbrook01@gmail.com>
2021-02-26 17:56:11 +0000

    Add renderC and refactor some classes

---
+++ src/main/java/org/bioshock/engine/entity/NetworkC.java
@@ -0,0 +1,18 @@
+package org.bioshock.engine.entity;
+
+public class NetworkC {
+    private boolean networked;
+
+    public NetworkC( boolean networked) {
+        this.networked = networked;
+    }
+
+
+	public boolean isNetworked() {
+		return networked;
+	}
+	
+	public void setNetworked(boolean networked) {
+		this.networked = networked;
+	}
+}
