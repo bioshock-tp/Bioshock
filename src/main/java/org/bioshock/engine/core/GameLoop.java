db5a01a5870dda9ca5654e4c257ae6bfffd62c7a
LukeAmos00 <l.amos00@hotmail.co.uk>
2021-03-02 04:39:52 +0000

    Added FPS counter

--- src/main/java/org/bioshock/engine/core/GameLoop.java
+++ src/main/java/org/bioshock/engine/core/GameLoop.java
@@ -6,19 +6,18 @@ import org.bioshock.engine.rendering.RenderManager;
 
 import javafx.animation.AnimationTimer;
 
-public final class GameLoop extends AnimationTimer{
-	
+public final class GameLoop extends AnimationTimer {
 	private long prev = 0;
-	
+
 	@Override
 	public void handle(long now) {
 		long nanoSDelta = now - prev;
 		double sDelta = nanoSDelta / 10e9;
-        
-		//might need a move player tick then a network tick then an update tick (need to know network implementation first)
+
 		NetworkManager.tick();
 		EntityManager.tick(sDelta);
 		RenderManager.tick();
+        FrameRate.tick(now);
 		prev = now;
 	}
 }
