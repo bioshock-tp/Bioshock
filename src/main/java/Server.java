23536d7337fe1bd4b04dd6d0879ce468658476ae
RaduMirceaAndrei00 <mirad28@yahoo.com>
2021-03-07 22:07:27 +0000

    Server made to work on host

--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -6,8 +6,8 @@ import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
 
 public class Server extends WebSocketServer {
-    private static final String host = "51.15.109.210";
-    private static final int port = 80;
+    //private static final String host = "51.15.109.210";
+    private static final int port = 8010;
 
     public Server(InetSocketAddress address) {
 		super(address);
@@ -17,50 +17,50 @@ public class Server extends WebSocketServer {
 	public void onOpen(WebSocket conn, ClientHandshake handshake) {
 		conn.send(Integer.toString(getConnections().size()));
 
-		System.out.println(String.format(
-            "New connection to %s",
-            conn.getRemoteSocketAddress()
-        ));
+		//System.out.println(String.format(
+            //"New connection to %s",
+            //conn.getRemoteSocketAddress()
+       //));
 	}
 
 	@Override
 	public void onClose(WebSocket conn, int code, String reas, boolean remote) {
-        System.out.println(String.format(
-            "Closed %s with exit code %d additional info: %s",
-            conn.getRemoteSocketAddress(),
-            code,
-            reas
-        ));
+        //System.out.println(String.format(
+        //    "Closed %s with exit code %d additional info: %s",
+        //    conn.getRemoteSocketAddress(),
+       //     code,
+       //     reas
+       // ));
 	}
 
 	@Override
 	public void onMessage(WebSocket conn, String message) {
-		System.out.println(String.format(
-            "Received message from %s: %s",
-            conn.getRemoteSocketAddress(),
-            message
-        ));
+		//System.out.println(String.format(
+           // "Received message from %s: %s",
+           // conn.getRemoteSocketAddress(),
+           // message
+       // ));
 
         broadcast(message);
 	}
 
 	@Override
 	public void onMessage( WebSocket conn, ByteBuffer message) {
-		System.out.println(String.format(
-            "Received ByteBuffer from %s: %s",
-            conn.getRemoteSocketAddress(),
-            message
-        ));
+		//System.out.println(String.format(
+            //"Received ByteBuffer from %s: %s",
+            //conn.getRemoteSocketAddress(),
+           // message
+        //));
 
         onMessage(conn, message.toString());
 	}
 
 	@Override
 	public void onError(WebSocket conn, Exception ex) {
-		System.out.println(String.format(
-            "An error occurred: %s",
-            ex
-        ));
+		//System.out.println(String.format(
+            //"An error occurred: %s",
+            //ex
+        //));
 	}
 
 	@Override
@@ -69,7 +69,8 @@ public class Server extends WebSocketServer {
 	}
 
 	public static void main(String[] args) {
-		WebSocketServer server = new Server(new InetSocketAddress(host, port));
+		//WebSocketServer server = new Server(new InetSocketAddress(host, port));
+		WebSocketServer server = new Server(new InetSocketAddress(port));
 		server.run();
 	}
 }
