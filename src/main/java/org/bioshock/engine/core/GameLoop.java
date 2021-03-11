97886775e9984572237223816a8c5cec27a36679
LukeAmos00 <l.amos00@hotmail.co.uk>
2021-03-11 15:10:49 +0000

    Fixed logic to 60 ticks per second

--- src/main/java/org/bioshock/engine/core/GameLoop.java
+++ src/main/java/org/bioshock/engine/core/GameLoop.java
@@ -3,22 +3,32 @@ package org.bioshock.engine.core;
 import org.bioshock.engine.entity.EntityManager;
 import org.bioshock.engine.networking.NetworkManager;
 import org.bioshock.engine.rendering.RenderManager;
+import org.bioshock.main.App;
 
 import javafx.animation.AnimationTimer;
 
 public final class GameLoop extends AnimationTimer {
+    private static final int LOGICRATE = 60;
+
 	private long prev = 0;
+    private long lastUpdate = 0;
 
 	@Override
 	public void handle(long now) {
 		long nanoSDelta = now - prev;
 		double sDelta = nanoSDelta / 10e9;
-
-        NetworkManager.tick();
-        EntityManager.tick(sDelta);
         RenderManager.tick();
         FrameRate.tick(now);
 
+        App.logger.debug(now);
+        App.logger.debug(now - lastUpdate);
+
+        if (now - lastUpdate >= LOGICRATE) {
+            NetworkManager.tick();
+            EntityManager.tick(sDelta);
+
+            lastUpdate = now;
+        }
 		prev = now;
 	}
 }
