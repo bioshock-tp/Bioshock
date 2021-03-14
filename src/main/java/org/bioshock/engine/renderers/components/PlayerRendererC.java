aec4cb5fc19d19e0f47b13c7b39aee59ee908964
LukeAmos00 <l.amos00@hotmail.co.uk>
2021-03-14 13:51:19 +0000

    Made game only after all players die

--- src/main/java/org/bioshock/engine/renderers/components/PlayerRendererC.java
+++ src/main/java/org/bioshock/engine/renderers/components/PlayerRendererC.java
@@ -1,15 +1,3 @@
 package org.bioshock.engine.renderers.components;
 
-import javafx.scene.paint.Color;
-
-public class PlayerRendererC extends SimpleRendererC {
-    private Color originalColour;
-
-    public void setOriginalColour(Color originalColour) {
-        this.originalColour = originalColour;
-    }
-
-    public void revertColour() {
-        setColor(originalColour);
-    }
-}
+public class PlayerRendererC extends SimpleRendererC {}
