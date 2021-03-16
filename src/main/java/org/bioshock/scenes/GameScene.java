710e1438ec1383f97dc862cbf730bec15219c6db
LukeAmos00 <l.amos00@hotmail.co.uk>
2021-03-16 13:04:36 +0000

    Added some documentation

--- src/main/java/org/bioshock/scenes/GameScene.java
+++ src/main/java/org/bioshock/scenes/GameScene.java
@@ -13,57 +13,147 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.StackPane;
 
+
+/**
+ * The base class for this games scenes
+ */
 public abstract class GameScene extends Scene {
+    /**
+     * The {@code StackPane} this scene adds to
+     */
     private StackPane pane;
+
+
+    /**
+     * The {@code Canvas} this scene draws on
+     */
     private Canvas canvas = new Canvas(
         WindowManager.getWindowWidth(),
         WindowManager.getWindowHeight()
     );
+
+
+    /**
+     * The entities that extends {@link Entity} to be drawn on the
+     * {@link #canvas}
+     */
     protected List<Entity> children = new ArrayList<>();
 
+
+    /**
+     * Creates a new {@code GameScene} using the {@code StackPane} provided by
+     * {@link #GameScene()}
+     * @param pane the {@code StackPane} to initialise the {@link #pane} field
+     * to
+     */
     private GameScene(StackPane pane) {
         super(pane);
         this.pane = pane;
         pane.getChildren().add(canvas);
     }
 
+
+    /**
+     * Initialises the {@code StackPane} to use in {@link #GameScene(StackPane)}
+     * so that it can be used in the {@code super} call and be assigned to the
+     * {@link #pane} field
+     */
     protected GameScene() {
         this(new StackPane());
     }
 
+
+    /**
+     * Initialises the scene and registers the scene's {@link #children} to
+     * both the {@link org.bioshock.engine.entity.EntityManager EntityManager}
+     * and the
+     * {@link org.bioshock.engine.rendering.RenderManager RenderManager}.
+     * Called in
+     * {@link org.bioshock.engine.scene.SceneManager#setScene(GameScene)
+     * SceneManager.setScene(GameScene)}
+     */
     public void initScene() {
         registerEntities();
         renderEntities();
     }
 
+
+    /**
+     * Registers the scene's {@link #children} to the
+     * {@link org.bioshock.engine.entity.EntityManager EntityManager}
+     * @see #renderEntities()
+     */
     public void registerEntities() {
         children.forEach(EntityManager::register);
 	}
 
+    /**
+     * Registers the scene's {@link #children} to the
+     * {@link org.bioshock.engine.rendering.RenderManager RenderManager}
+     * @see #registerEntities()
+     */
 	public void renderEntities() {
         children.forEach(RenderManager::register);
 	}
 
+
+    /**
+     * Unregisters the scene's {@link #children} from the
+     * {@link org.bioshock.engine.entity.EntityManager EntityManager}
+     * @see #destroyEntities()
+     */
     public void unregisterEntities() {
         children.forEach(EntityManager::unregister);
 	}
 
+
+    /**
+     * Unregisters the scene's {@link #children} from the
+     * {@link org.bioshock.engine.rendering.RenderManager RenderManager}
+     * @see #unregisterEntities()
+     */
 	public void destroyEntities() {
         children.forEach(RenderManager::unregister);
 	}
 
+
+    /**
+     * Sets the background of the scene
+     * @param background the {@code Background} of the scene
+     */
     public void setBackground(Background background) {
         pane.setBackground(background);
 	}
 
+
+    /**
+     * @return StackPane the scene's {@link #pane}
+     * @see #getCanvas()
+     */
     public StackPane getPane() {
         return pane;
     }
 
-	public Canvas getCanvas() {
+
+    /**
+     * @return Canvas the scene's {@link #canvas}
+     * @see #getPane()
+     * @see Canvas#getGraphicsContext2D()
+     */
+    public Canvas getCanvas() {
 		return canvas;
 	}
 
+
+    /**
+     * Destroys the scene and unregisters the scene's {@link #children} from
+     * both the {@link org.bioshock.engine.entity.EntityManager EntityManager}
+     * and the
+     * {@link org.bioshock.engine.rendering.RenderManager RenderManager}.
+     * Called in
+     * {@link org.bioshock.engine.scene.SceneManager#setScene(GameScene)
+     * SceneManager.setScene(GameScene)}
+     */
 	public void destroy() {
         unregisterEntities();
         destroyEntities();
