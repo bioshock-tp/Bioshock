b0c6d5efc847a4b0344d7c5875814b17189d18c2
WillHolbrook <William.d.holbrook01@gmail.com>
2021-04-06 15:34:17 +0100

    make traversable nodes only connect in 4 not 8 directions

--- src/main/java/org/bioshock/entities/map/TraversableEdgeGenerator.java
+++ src/main/java/org/bioshock/entities/map/TraversableEdgeGenerator.java
@@ -1,5 +1,10 @@
 package org.bioshock.entities.map;
 
+import static org.bioshock.utils.Direction.EAST;
+import static org.bioshock.utils.Direction.NORTH;
+import static org.bioshock.utils.Direction.SOUTH;
+import static org.bioshock.utils.Direction.WEST;
+
 import org.bioshock.engine.pathfinding.EdgeGenerator;
 import org.bioshock.engine.pathfinding.GraphNode;
 import org.bioshock.utils.Direction;
@@ -10,7 +15,10 @@ public class TraversableEdgeGenerator implements EdgeGenerator<GraphNode, Pair<D
 
 	@Override
 	public Pair<Direction, Double> getEdgeInfo(GraphNode source, GraphNode dest, Direction d) {
-		Double distance = dest.getLocation().subtract(source.getLocation()).magnitude();
-		return new Pair<>(d, distance);
+		if(d == NORTH || d == EAST || d == SOUTH || d == WEST) {
+			Double distance = dest.getLocation().subtract(source.getLocation()).magnitude();
+			return new Pair<>(d, distance);
+		}
+		return null;
 	}
 }
