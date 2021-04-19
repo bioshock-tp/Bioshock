8fc555ed7253cb7f103bb0a9a0a4f0129a89b780
WillHolbrook <William.d.holbrook01@gmail.com>
2021-04-19 14:25:24 +0100

    Add ChatManager and suspending other inputs

---
+++ src/main/java/org/bioshock/engine/core/ChatManager.java
@@ -0,0 +1,44 @@
+package org.bioshock.engine.core;
+
+import org.bioshock.main.App;
+
+public class ChatManager {
+
+    private static boolean inChat = false;
+    private static StringBuilder currentText = new StringBuilder();
+    private static boolean sendMessage = false;
+    private static String textToSend;
+    
+    public static boolean isSendMessage() {
+        return sendMessage;
+    }
+    
+    public static void setSendMessage(boolean sendMessage) {
+        ChatManager.sendMessage = sendMessage;
+    }
+    
+    public static String getTextToSend() {
+        return textToSend;
+    }
+    
+    public static boolean isInChat() {
+        return inChat;
+    }
+    
+    public static void setInChat(boolean inChat) {
+        ChatManager.inChat = inChat;
+    }
+    
+    public static StringBuilder getStrBuild() {
+        return currentText;
+    }
+    
+    public static String popText() {
+        textToSend = currentText.toString();
+        App.logger.debug("Text to Send: " + textToSend);
+        sendMessage = true;
+        currentText.setLength(0);
+        return textToSend;
+    }
+    
+}
