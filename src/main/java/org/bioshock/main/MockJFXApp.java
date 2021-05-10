9d0f573bacd7507db03660f4c7619995de8ba88f
WillHolbrook <William.d.holbrook01@gmail.com>
2021-05-11 00:33:00 +0100

    Start to test GenericMap

---
+++ src/main/java/org/bioshock/main/MockJFXApp.java
@@ -0,0 +1,17 @@
+package org.bioshock.main;
+
+import java.util.concurrent.Semaphore;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MockJFXApp extends Application{    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+    }
+    
+    public void testLaunch() {
+        launch("");
+    }
+
+}
---
+++ src/test/java/org/bioshock/entities/map/maps/GenericMapTest.java
@@ -0,0 +1,50 @@
+package org.bioshock.entities.map.maps;
+
+import static org.bioshock.entities.map.utils.RoomType.NO_ROOM;
+import static org.bioshock.entities.map.utils.RoomType.SINGLE_ROOM;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
+import org.bioshock.JavaFxTest;
+import org.bioshock.entities.map.utils.RoomType;
+import org.bioshock.main.App;
+import org.bioshock.main.MockJFXApp;
+import org.bioshock.scenes.SceneManager;
+import org.bioshock.utils.GlobalConstants;
+import org.bioshock.utils.Size;
+import org.junit.jupiter.api.Test;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+
+import javafx.application.Application;
+import javafx.geometry.Point3D;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+
+class GenericMapTest extends JavaFxTest{
+
+    
+    @Test
+    @PrepareForTest(GlobalConstants.class)
+    void test() throws InterruptedException{
+        init();
+        
+        Map map = new GenericMap(
+            new Point3D(0, 0, 0),
+            1,
+            new Size(5, 7),
+            new Size(3, 5),
+            Color.SADDLEBROWN,
+            GlobalConstants.TEST_MAP,
+            0
+        );
+        
+        SceneManager.setMap(map);
+        
+        assertTrue(true);
+    }
+
+}
